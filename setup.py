cc8fa4a77e9e27a42623353f289f5eec0fd6b86c
Maksim Terpilowski <maximtrp@gmail.com>
2021-03-21 21:06:58 +0300

    setup files updated

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 from platform import system
+# from numpy import get_include
 
 extra_link_args = ['-lomp'] if system() == 'Darwin' else ['-fopenmp']
 extra_compile_args = ['-Xpreprocessor', '-fopenmp']\
@@ -11,6 +12,8 @@ ext_modules = [
     Extension(
         "bitermplus._btm",
         sources=["src/bitermplus/_btm.pyx"],
+        # include_dirs=[get_include()],
+        # library_dirs=[get_include()],
         extra_compile_args=extra_compile_args,
         extra_link_args=extra_link_args),
     Extension(
